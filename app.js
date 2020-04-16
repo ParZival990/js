2522e59cd5260a62b265dcb4cc9519c9a83bb306
ParZival990 <63736646+ParZival990@users.noreply.github.com>
2020-04-15 21:29:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,73 @@
+alert("Bienvenid@");
+let f= prompt("Por favor digita la cantidad de (filas) que quieres guardar en el arreglo bidimencional ",);
+let c = prompt("Por favor digita la cantidad de (columnas) que quiere ingresar en el arreglo bidimencional ",);
 
+// let mt = [f][c];
+//array bd [1]
+let arrayB= new Array(f);
+for (let i = 0; i < f; i++) {
+   arrayB[i] = new Array(c);
+}
 
-let f= prompt("Por favor digita la cantidad de filas que quiere que tenga el arreglo\n multidimencional ",);
-let c = prompt("Por favor digita la cantidad de columnas que quiere que tenga el \narreglomultidimencional ",);
+//array bd [2]
+let arrayB2= new Array(f);
+for (let i = 0; i < f; i++) {
+    arrayB2[i] = new Array(c);
+ }
 
-let mt = [f][c];
-
+//----------------------------------------------------------------------------------------------
+//llenado array bd 1 
 alert("Ahora hay que llenar la matriz [1]");
-for(let i=0;i<f;i++){	//lleno la matriz 
-    for(let j=0;j<c;j++){
-      mt=[i][j]=prompt("digita numeros para llenar las filas y las columnas de la matiz. Ps["+i+"]["+j+"]: ",);
+for (let i = 0; i < f; i++){
+    for(let j = 0; j < c; j++){
+        arrayB[i][j] = Number =prompt("digita numeros para llenar las filas y las columnas de la matiz[1].\nPs["+i+"]["+j+"]:",);
     }	
 }
 
-for(let i=0;i<f;i++){	//lleno la matriz 
-    for(let j=0;j<c;j++){
-        document.write("\n");
-        document.write(mt[i][j]);
+//----------------------------------------------------------------------------------------------
+//llenando array bd 2
+alert("Ahora hay que llenar el matriz [2]");
+for (let i = 0; i < f; i++){
+    for(let j = 0; j < c; j++){
+        arrayB2[i][j]= Number= prompt("digita numeros para llenar las filas y las columnas de la matiz[2].\nPs["+i+"]["+j+"]:",);
     }	
 }
 
+//Multiplicacion
+document.write("<h1>Producto de dos Arreglos Bidimencionales</h1>");
+document.write("</br>");
+document.write
+("---------------------------------------Multiplicacion---------------------------------------");
+
+document.write("</br>");
+for (let i = 0; i < f; i++){
+    for(let j = 0; j < c; j++){
+        document.write("</br>"+"el resultado del producto de las posiciones "+"["+i+"]"+"["+j+"]"+" de las matrices es: " +(arrayB[i][j]*arrayB2[i][j]));
+   }
+   	document.write("\n");
+}
+
+document.write("</br>");
+document.write("</br>");
+document.write
+("--------------------------------------La matriz [1]-----------------------------------------");
+
+document.write("</br>");
+for (let i = 0; i < f; i++){
+    for(let j = 0; j < c; j++){
+        document.write("["+arrayB[i][j]+"]");
+    }	
+    document.write("</br>"); 
+}
+
+document.write("</br>");
+document.write
+("-------------------------------------La matriz [2]------------------------------------------");
+
+document.write("</br>");
+for (let i = 0; i < f; i++){
+    for(let j = 0; j < c; j++){
+        document.write("["+arrayB2[i][j]+"]");
+    }	
+    document.write("</br>"); 
+}
